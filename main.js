5d5f6fbe54cc7084e37122841d0b000868e2a5ff
ubaidi10 <17-ECT-10@students.uettaxila.edu.pk>
2024-11-20 11:42:41 +0500

    Scroll Reveal Script

--- main.js
+++ main.js
@@ -28,3 +28,13 @@ const observer = new IntersectionObserver(entries => {
 sections.forEach(section => {
     observer.observe(section);
 });
+/* Scroll Reveal */
+ScrollReveal({
+    distance: '80px',
+    duration:2000,
+    delay: 200,
+ });
+ ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ ScrollReveal().reveal('.home-img, .services-container,.portfolio-box, .contact form', { origin: 'bottom' });
+ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
