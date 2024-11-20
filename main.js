12ad8d7e7a9b91ff283551a0711455f77c4bf364
ubaidi10 <17-ECT-10@students.uettaxila.edu.pk>
2024-11-20 11:43:12 +0500

    Typed JS Script Added

--- main.js
+++ main.js
@@ -38,3 +38,11 @@ ScrollReveal({
  ScrollReveal().reveal('.home-img, .services-container,.portfolio-box, .contact form', { origin: 'bottom' });
  ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
  ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ /* Typed JS */
+ const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Web Designer', 'Electronics Engineer'],
+    typeSpeed:70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+ });
